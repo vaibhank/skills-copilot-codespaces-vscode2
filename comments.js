5532e1b332828b1c6a73e7fc011b65c3556594e3
vaibhank <vaibhank@yahoo.com>
2025-02-25 00:10:32 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,43 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-var commentsPath = path.join(__dirname, 'comments.json');
-var comments = JSON.parse(fs.readFileSync(commentsPath, 'utf8'));
+// create a route
+// create a middleware
+// use the middleware
+// use the route
+// get the comments from the file
+// add comments to the file
+// render the comments
 
-// middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
 
-// routes
-app.get('/comments', function(req, res) {
+app.use(express.json());
+
+const commentsPath = path.join(__dirname, 'comments.json');
+
+const getComments = () => {
+  const comments = fs.readFileSync(commentsPath);
+  return JSON.parse(comments);
+};
+
+const addComment = (comment) => {
+  const comments = getComments();
+  comments.push(comment);
+  fs.writeFileSync(commentsPath, JSON.stringify(comments));
+};
+
+app.get('/comments', (req, res) => {
+  const comments = getComments();
   res.send(comments);
 });
 
-app.post('/comments', function(req, res) {
-  comments.push(req.body);
-  fs.writeFile(commentsPath, JSON.stringify(comments, null, 4), function(err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.send(comments);
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  addComment(comment);
+  res.send('Comment added');
 });
 
-// start server
-app.listen(3000, function() {
-  console.log('Server started on port 3000');
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
