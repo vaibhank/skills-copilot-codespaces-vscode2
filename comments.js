d28ceabd030599a153ae7e557ac62d2754401ced
vaibhank <vaibhank@yahoo.com>
2025-02-25 00:01:16 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,33 @@
 // create web server
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+var comments = JSON.parse(fs.readFileSync(commentsPath, 'utf8'));
 
-// create a route
-app.get('/comments', (req, res) => {
-    res.send('You are on the comments page');
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+// routes
+app.get('/comments', function(req, res) {
+  res.send(comments);
+});
+
+app.post('/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile(commentsPath, JSON.stringify(comments, null, 4), function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.send(comments);
 });
 
 // start server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
 });
